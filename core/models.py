15fef4684b99552f4351e17d969520a092693dfd
ugen <ugen4321@gmail.com>
2020-08-07 09:54:48 +0300

    5

--- core/models.py
+++ core/models.py
@@ -11,3 +11,37 @@ class Category(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Product(models.Model):
+    brand = models.ForeignKey('Brand', on_delete=models.CASCADE)
+    price = models.FloatField()
+
+
+class Sneaker(Product):
+    SUBTYPE_1 = '1'
+    SUBTYPE_2 = '2'
+
+    SNEAKER_CHOICES = (
+        (SUBTYPE_1, '1'),
+        (SUBTYPE_2, '2')
+    )
+    subtype = models.CharField(choices=SNEAKER_CHOICES, max_length=1)
+
+
+class Bag(Product):
+    pass
+
+
+class Brand(models.Model):
+    title = models.CharField(max_length=32)
+
+
+class BrandModel(models.Model):
+    title = models.CharField(max_length=32)
+    brand = models.ForeignKey(Brand, on_delete=models.CASCADE)
+
+
+class Sale(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    sale_date = models.DateTimeField()
